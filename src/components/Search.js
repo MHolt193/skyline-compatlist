a6b1602fd9f4cde5828900a78ad002aae6984734
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-10-02 12:18:57 -0600

    formatting and turn autocomplete off

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
 import React from "react";
-import classes from "./Search.module.css"
+import classes from "./Search.module.css";
 
 const Search = (props) => {
   return (
     <div>
       <form onSubmit={props.searchHandler} className={classes.searchContainer}>
-        <input id="search" type="search" placeholder="Search Game Title" className={classes.searchBar} />
-        <button type="submit" className={classes.searchBtn}>Search</button>
+        <input
+          id="search"
+          type="search"
+          placeholder="Search Game Title"
+          className={classes.searchBar}
+          autocomplete="off"
+        />
+        <button type="submit" className={classes.searchBtn}>
+          Search
+        </button>
       </form>
     </div>
   );
